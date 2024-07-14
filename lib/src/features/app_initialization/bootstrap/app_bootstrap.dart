af0f7c5fbcee46ed631a943f3dae78ab7e400394
Umut Aktaş <aktasumut@gmail.com>
2024-07-14 21:05:22 +0200

    update imports

--- lib/src/features/app_initialization/bootstrap/app_bootstrap.dart
+++ lib/src/features/app_initialization/bootstrap/app_bootstrap.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:real_estate/main.dart';
+import 'package:real_estate/src/app.dart';
 import 'package:real_estate/src/exceptions/async_error_logger.dart';
 import 'package:real_estate/src/features/app_initialization/presentation/app_startup_page.dart';
 
