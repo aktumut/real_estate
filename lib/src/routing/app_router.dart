8ebdf6d857ee382bee0721a8bbe19ba135288c60
Umut Aktaş <aktasumut@gmail.com>
2024-07-14 21:05:32 +0200

    add HouseFeedPage

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:go_router/go_router.dart';
 import 'package:real_estate/src/constants/page_paths.dart';
+import 'package:real_estate/src/features/shop/presentation/house_feed/house_feed_page.dart';
 import 'package:real_estate/src/routing/not_found_page.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -9,7 +10,11 @@ final goRouterProvider = Provider<GoRouter>((ref) {
     initialLocation: PagePaths.houseFeed,
     debugLogDiagnostics: kDebugMode,
     routes: [
-      GoRoute(path: PagePaths.houseFeed, name: PageNames.houseFeed),
+      GoRoute(
+        path: PagePaths.houseFeed,
+        name: PageNames.houseFeed,
+        builder: (context, state) => const HouseFeedPage(),
+      ),
     ],
     errorBuilder: (context, state) => const NotFoundPage(),
   );
