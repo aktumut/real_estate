38318255b7ef963a0f9408f9bc8fd20698dc9609
Umut Aktaş <aktasumut@gmail.com>
2024-07-20 14:17:06 +0200

    add app bar, sample house card

--- lib/src/features/shop/presentation/house_feed/house_feed_page.dart
+++ lib/src/features/shop/presentation/house_feed/house_feed_page.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:real_estate/src/constants/app_sizes.dart';
+import 'package:real_estate/src/features/shop/presentation/house_feed/house_card/house_card.dart';
+import 'package:real_estate/src/widgets_common/custom_app_bar.dart';
 
 class HouseFeedPage extends StatelessWidget {
   const HouseFeedPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      appBar: CustomAppBar(),
+      body: SafeArea(
+        child: Padding(
+          padding: Paddings.scaffoldAll,
+          child: Column(
+            children: [
+              gapH16,
+              HouseCard(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
