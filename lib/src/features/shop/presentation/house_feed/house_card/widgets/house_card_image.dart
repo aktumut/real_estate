d1a1c0818e7431f73bc021b228758ff8ecc4fb75
Umut Aktaş <aktasumut@gmail.com>
2024-07-21 16:45:25 +0200

    move

--- lib/src/features/shop/presentation/house_feed/house_card/widgets/house_card_image.dart
+++ lib/src/features/shop/presentation/house_feed/house_card/widgets/house_card_image.dart
@@ -2,7 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:real_estate/src/constants/app_sizes.dart';
 import 'package:real_estate/src/constants/strings.dart';
-import 'package:real_estate/src/utilities/assets_library.dart';
+import 'package:real_estate/src/utils/assets_library.dart';
 import 'package:real_estate/src/widgets_common/custom_circular_progress_indicator.dart';
 
 class HouseCardImage extends StatelessWidget {
