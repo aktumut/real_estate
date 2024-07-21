6cc4cf12aa9649858690084d87ece0acc8952713
Umut Aktaş <aktasumut@gmail.com>
2024-07-21 16:44:00 +0200

    add searchResult

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -2,7 +2,9 @@ import 'package:flutter/foundation.dart';
 import 'package:go_router/go_router.dart';
 import 'package:real_estate/src/constants/keys.dart';
 import 'package:real_estate/src/constants/page_paths.dart';
+import 'package:real_estate/src/constants/strings.dart';
 import 'package:real_estate/src/features/credentials/presentation/credentials_page.dart';
+import 'package:real_estate/src/features/search/presentation/search_result_page.dart';
 import 'package:real_estate/src/features/shop/presentation/house_feed/house_feed_page.dart';
 import 'package:real_estate/src/routing/not_found_page.dart';
 import 'package:real_estate/src/routing/scaffold_with_nested_navigation.dart';
@@ -31,6 +33,19 @@ final goRouterProvider = Provider<GoRouter>((ref) {
                 pageBuilder: (context, state) => const NoTransitionPage(
                   child: HouseFeedPage(),
                 ),
+              ),
+              GoRoute(
+                path: PagePaths.searchResult,
+                name: PageNames.searchResult,
+                pageBuilder: (context, state) {
+                  final searchQuery = state.pathParameters[tTextSearchQuery];
+
+                  return NoTransitionPage(
+                    child: SearchResultPage(
+                      searchQuery: searchQuery.toString(),
+                    ),
+                  );
+                },
               ),
             ],
           ),
