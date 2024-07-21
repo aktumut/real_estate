3010338a03665bc763529d89c6b401fe3b3eca5c
Umut Aktaş <aktasumut@gmail.com>
2024-07-21 16:44:59 +0200

    add SearchBarWidget

--- lib/src/widgets_common/custom_app_bar.dart
+++ lib/src/widgets_common/custom_app_bar.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:real_estate/src/constants/app_sizes.dart';
 import 'package:real_estate/src/extensions/app_localizations_context.dart';
+import 'package:real_estate/src/features/search/presentation/widgets/search_bar_widget.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomAppBar({
-    super.key,
-  });
+  const CustomAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return PreferredSize(
-      preferredSize: const Size.fromHeight(Sizes.p48),
+      preferredSize: const Size.fromHeight(Sizes.p96),
       child: AppBar(
-        title: Transform(
-          transform: Matrix4.translationValues(0, Sizes.p8, 0),
-          child: Text(context.loc.appBarTitle),
+        title: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(context.loc.appBarTitle),
+            gapH16,
+            const SearchBarWidget(),
+          ],
         ),
         centerTitle: false,
       ),
@@ -22,5 +27,5 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(Sizes.p32);
+  Size get preferredSize => const Size.fromHeight(Sizes.p96);
 }
