60f5c801a8df3db3339687fe0b2e16e46c809b25
Umut Aktaş <aktasumut@gmail.com>
2024-07-28 20:52:59 +0200

    add IsLikedWidget

--- lib/src/features/shop/presentation/house_detail/widgets/house_detail_image_widget.dart
+++ lib/src/features/shop/presentation/house_detail/widgets/house_detail_image_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:real_estate/src/constants/app_sizes.dart';
 import 'package:real_estate/src/constants/strings.dart';
 import 'package:real_estate/src/features/shop/domain/house_model.dart';
+import 'package:real_estate/src/features/wish_list/presentation/widgets/is_liked_widget.dart';
 import 'package:real_estate/src/utils/assets_library.dart';
 import 'package:real_estate/src/widgets_common/custom_circular_progress_indicator.dart';
 
@@ -17,18 +18,27 @@ class HouseDetailImageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      child: SizedBox(
-        width: double.infinity,
-        height: Sizes.heightHouseDetailImage,
-        child: CachedNetworkImage(
-          imageUrl: '$tTextBaseUrl${house.image}',
-          fit: BoxFit.cover,
-          progressIndicatorBuilder: (context, url, downloadProgress) =>
-              CustomCircularProgressIndicator(
-            value: downloadProgress.progress,
+      child: Stack(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: Sizes.heightHouseDetailImage,
+            child: CachedNetworkImage(
+              imageUrl: '$tTextBaseUrl${house.image}',
+              fit: BoxFit.cover,
+              progressIndicatorBuilder: (context, url, downloadProgress) =>
+                  CustomCircularProgressIndicator(
+                value: downloadProgress.progress,
+              ),
+              errorWidget: (context, url, error) => IconLibrary.closeIcon,
+            ),
           ),
-          errorWidget: (context, url, error) => IconLibrary.closeIcon,
-        ),
+          Positioned(
+            top: Sizes.heightHouseDetailImage - Sizes.p64,
+            right: Sizes.p20,
+            child: IsLikedWidget(house: house, isHouseDetailPage: true),
+          ),
+        ],
       ),
     );
   }
